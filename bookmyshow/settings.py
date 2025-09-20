89a8a95ffcc631b70070cca8c5ed9159da0ebb69
C Veena Madhuri <veenamadhuric7@gmail.com>
2025-09-20 22:25:46 +0530

    Update settings.py

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
-    'bookmyshow.onrender.com'
+    'localhost',
+    '127.0.0.1',
+    '.vercel.app',
 ]
 
 
